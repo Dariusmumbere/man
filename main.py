584770953ce9fa1b09f0c4ce705ad31d9965065e
Dariusmumbere <dariusmumbere@gmail.com>
2025-04-26 15:56:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import UploadFile, File, Form
 from fastapi.responses import FileResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, File
 import os
 import psycopg2
 import logging
